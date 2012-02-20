2a7cf469ac15f978252e6c71c615f78052097ef1
Bruno Binet <bruno.binet@gmail.com>
2012-02-20 23:44:36 +0100

    make use of mapfish protocol using Papyrus

--- papyrussample/__init__.py
+++ papyrussample/__init__.py
@@ -1,6 +1,7 @@
 from pyramid.config import Configurator
 from pyramid.mako_templating import renderer_factory as mako_renderer_factory
 from papyrus.renderers import GeoJSON
+import papyrus
 from sqlalchemy import engine_from_config
 
 from .models import DBSession
@@ -12,14 +13,16 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
 
+    # include Papyrus config
+    config.include(papyrus.includeme)
+
     # bind the mako renderer to other file extensions
     config.add_renderer('.mako', mako_renderer_factory)
     config.add_renderer('geojson', GeoJSON())
 
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('poi_count', '/pois/count')
-    config.add_route('pois', '/pois')
+    config.add_papyrus_routes('pois', '/pois')
     config.scan()
     return config.make_wsgi_app()
 
--- papyrussample/views.py
+++ papyrussample/views.py
@@ -1,19 +1,42 @@
 from pyramid.view import view_config
+from papyrus.protocol import Protocol
 
 from .models import (
     DBSession,
     Poi
     )
 
+
+proto = Protocol(DBSession, Poi, 'the_geom')
+
+@view_config(route_name='pois_read_many', renderer='geojson')
+def read_many(request):
+    return proto.read(request)
+
+@view_config(route_name='pois_read_one', renderer='geojson')
+def read_one(request):
+    id = request.matchdict.get('id', None)
+    return proto.read(request, id=id)
+
+@view_config(route_name='pois_count', renderer='string')
+def count(request):
+    return proto.count(request)
+
+@view_config(route_name='pois_create', renderer='geojson')
+def create(request):
+    return proto.create(request)
+
+@view_config(route_name='pois_update', renderer='geojson')
+def update(request):
+    id = request.matchdict['id']
+    return proto.update(request, id)
+
+@view_config(route_name='pois_delete')
+def delete(request):
+    id = request.matchdict['id']
+    return proto.delete(request, id)
+
+
 @view_config(route_name='home', renderer='index.html.mako')
 def index(request):
     return {}
-
-@view_config(route_name='pois', renderer='geojson')
-def pois(request):
-    pois = DBSession.query(Poi).limit(2)
-    return pois.all()
-
-@view_config(route_name='poi_count', renderer='string')
-def poi_count(request):
-    return DBSession.query(Poi).count()
