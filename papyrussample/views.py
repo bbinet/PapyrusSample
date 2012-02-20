a4be2402a7298378bba1cd831cf920280e4126c1
Bruno Binet <bruno.binet@gmail.com>
2012-02-20 23:22:28 +0100

    add poi count webservice

--- papyrussample/views.py
+++ papyrussample/views.py
@@ -1,5 +1,14 @@
 from pyramid.view import view_config
 
+from .models import (
+    DBSession,
+    Poi
+    )
+
 @view_config(route_name='home', renderer='index.html.mako')
 def index(request):
     return {}
+
+@view_config(route_name='poi_count', renderer='string')
+def poi_count(request):
+    return DBSession.query(Poi).count()
