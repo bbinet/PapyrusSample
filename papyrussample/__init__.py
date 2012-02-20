9f6d092db5d4f5a5876603cc6af1fb144a5e8d72
Bruno Binet <bruno.binet@gmail.com>
2012-02-20 22:14:39 +0100

    make use of mako template engine instead of chameleon

--- papyrussample/__init__.py
+++ papyrussample/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.mako_templating import renderer_factory as mako_renderer_factory
 from sqlalchemy import engine_from_config
 
 from .models import DBSession
@@ -9,6 +10,10 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
+
+    # bind the mako renderer to other file extensions
+    config.add_renderer('.mako', mako_renderer_factory)
+
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
