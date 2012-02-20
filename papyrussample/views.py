059eff78d78e61c2ae74553fa41ac19e6f06acd6
Bruno Binet <bruno.binet@gmail.com>
2012-02-20 22:42:39 +0100

    cleanup MyModel stuff and remove pylons static files

--- papyrussample/views.py
+++ papyrussample/views.py
@@ -1,11 +1,5 @@
 from pyramid.view import view_config
 
-from .models import (
-    DBSession,
-    MyModel,
-    )
-
 @view_config(route_name='home', renderer='index.html.mako')
 def index(request):
-    one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
-    return {'one':one, 'project':'PapyrusSample'}
+    return {}
