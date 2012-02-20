f705322aad42c13c52193744e1de92b14b7c28ac
Bruno Binet <bruno.binet@gmail.com>
2012-02-20 23:18:15 +0100

    create sqlalchemy Poi model

--- papyrussample/models.py
+++ papyrussample/models.py
@@ -1,12 +1,25 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Unicode,
+    )
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
     )
-
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
+
+class Poi(Base):
+    __tablename__ = 'poi_osm'
+    gid = Column(Integer, primary_key=True)
+    rating  = Column(Integer)
+    name = Column(Unicode(250))
+    code_dept = Column(Unicode(100))
+    type_desc = Column(Unicode(254))
+    type = Column(Unicode(20))
+    theme = Column(Unicode(250))
 
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'SQLAlchemy',
+    'psycopg2',
     'transaction',
     'pyramid_tm',
     'pyramid_debugtoolbar',
